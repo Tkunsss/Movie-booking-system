a579e262dff95847ae2f74b077a0e0e9a23f6830
tikunss <135447187+Tkunsss@users.noreply.github.com>
2026-02-03 08:10:22 +0700

    Update Movie.java

---
+++ src/model/Movie.java
@@ -0,0 +1,19 @@
+package model;
+
+public class Movie {
+    private String title;
+    private double price;
+
+    public Movie(String title, double price) {
+        this.title = title;
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
